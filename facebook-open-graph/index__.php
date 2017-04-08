645b60399fc429738d9938a1adc9bd334bb1f256
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-04-08 23:51:49 +0700

    LAUNCH!

---
+++ facebook-open-graph/index__.php
@@ -0,0 +1,19 @@
+<?php
+
+function fn_facebook_open_graph_replace($content) {
+    if ($page = Lot::get('page')) {
+        global $site, $url;
+        $html  = '<!-- Begin Facebook Open Graph -->';
+        $html .= '<meta property="og:title" content="' . To::text($site->page->title) . '">';
+        $html .= '<meta property="og:url" content="' . $url->current . '">';
+        $html .= '<meta property="og:description" content="' . To::text($page->description($site->description)) . '">';
+        $html .= '<meta property="og:image" content="' . $page->image($url . '/favicon.ico') . '">';
+        $html .= '<meta property="og:site_name" content="' . To::text($site->title) . '">';
+        $html .= '<meta property="og:type" content="' . ($site->type === 'page' ? 'article' : 'website') . '">';
+        $html .= '<!-- End Facebook Open Graph -->';
+        return str_ireplace('</head>', $html . '</head>', $content);
+    }
+    return $content;
+}
+
+Hook::set('shield.output', 'fn_facebook_open_graph_replace', 1.9);
