790b6f039ec5323248cf33ec645f3a92ddf2f293
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-12-17 20:49:26 +0700

    For Mecha 2.2.0

--- facebook-open-graph/index.php
+++ facebook-open-graph/index.php
@@ -1,19 +1,19 @@
-<?php
+<?php namespace fn;
 
-function fn_facebook_open_graph($content) {
-    if ($page = Lot::get('page')) {
-        global $site, $url;
-        $html  = '<!-- Begin Facebook Open Graph -->';
-        $html .= '<meta property="og:title" content="' . To::text($site->page->title) . '">';
-        $html .= '<meta property="og:url" content="' . $url->current . '">';
-        $html .= '<meta property="og:description" content="' . To::text($page->description($site->description)) . '">';
-        $html .= '<meta property="og:image" content="' . $page->image($url . '/favicon.ico') . '">';
-        $html .= '<meta property="og:site_name" content="' . To::text($site->title) . '">';
-        $html .= '<meta property="og:type" content="' . ($site->type === 'page' ? 'article' : 'website') . '">';
-        $html .= '<!-- End Facebook Open Graph -->';
-        return str_ireplace('</head>', $html . '</head>', $content);
+function facebook_open_graph($content) {
+    extract(\Lot::get());
+    if (!empty($page)) {
+        $out  = '<!-- Begin Facebook Open Graph -->';
+        $out .= '<meta property="og:title" content="' . \To::text($site->trace) . '">';
+        $out .= '<meta property="og:url" content="' . $url->current . '">';
+        $out .= '<meta property="og:description" content="' . \To::text($page->description ?: $config->description) . '">';
+        $out .= '<meta property="og:image" content="' . ($page->image ?: $url . '/favicon.ico') . '">';
+        $out .= '<meta property="og:site_name" content="' . \To::text($config->title) . '">';
+        $out .= '<meta property="og:type" content="' . ($config->is('page') ? 'article' : 'website') . '">';
+        $out .= '<!-- End Facebook Open Graph -->';
+        return str_replace('</head>', $out . '</head>', $content);
     }
     return $content;
 }
 
-Hook::set('shield.yield', 'fn_facebook_open_graph', 1.9);
+\Hook::set('shield.yield', __NAMESPACE__ . "\\facebook_open_graph", 1.9);
