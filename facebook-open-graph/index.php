0dd700651a17d53014ec8fe71dd57e859c169ae9
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-05-08 11:06:32 +0700

    #54

--- facebook-open-graph/index.php
+++ facebook-open-graph/index.php
@@ -1,13 +1,13 @@
-<?php namespace fn;
+<?php namespace _;
 
 function facebook_open_graph($content) {
-    extract(\Lot::get(), \EXTR_SKIP);
+    extract($GLOBALS, \EXTR_SKIP);
     if (!empty($page)) {
         $out  = '<!-- Begin Facebook Open Graph -->';
         $out .= '<meta property="og:title" content="' . \To::text($site->trace) . '">';
-        $out .= '<meta property="og:url" content="' . $url->current . '">';
-        $out .= '<meta property="og:description" content="' . \To::text($page->description ?: $config->description) . '">';
-        $out .= '<meta property="og:image" content="' . ($page->image ?: $url . '/favicon.ico') . '">';
+        $out .= '<meta property="og:url" content="' . \strtr($url->current, ['&' => '&amp;']) . '">';
+        $out .= '<meta property="og:description" content="' . \To::text($page->description ?? $config->description) . '">';
+        $out .= '<meta property="og:image" content="' . ($page->image ?? $url . '/favicon.ico') . '">';
         $out .= '<meta property="og:site_name" content="' . \To::text($config->title) . '">';
         $out .= '<meta property="og:type" content="' . ($config->is('page') ? 'article' : 'website') . '">';
         $out .= '<!-- End Facebook Open Graph -->';
