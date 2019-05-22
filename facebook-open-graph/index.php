887dfab1a1ade3ef1def275323a95c5063e51184
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-05-22 10:50:04 +0700

    Update index.php

--- facebook-open-graph/index.php
+++ facebook-open-graph/index.php
@@ -4,9 +4,11 @@ function facebook_open_graph($content) {
     extract($GLOBALS, \EXTR_SKIP);
     if (!empty($page)) {
         $out  = '<!-- Begin Facebook Open Graph -->';
-        $out .= '<meta property="og:title" content="' . \w($site->trace) . '">';
+        $out .= '<meta property="og:title" content="' . \w($t->reverse) . '">';
         $out .= '<meta property="og:url" content="' . \r('&', '&amp;', $url->current) . '">';
-        $out .= '<meta property="og:description" content="' . \w($page->description ?? $config->description) . '">';
+        if ($w = \w($page->description ?? $config->description ?? "")) {
+            $out .= '<meta property="og:description" content="' . $w . '">';
+        }
         $out .= '<meta property="og:image" content="' . ($page->image ?? $url . '/favicon.ico') . '">';
         $out .= '<meta property="og:site_name" content="' . \w($config->title) . '">';
         $out .= '<meta property="og:type" content="' . ($config->is('page') ? 'article' : 'website') . '">';
