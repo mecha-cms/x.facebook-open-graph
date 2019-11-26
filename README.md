3f037f210dff324cd32f92543f0779d0cbaf046f
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-11-26 23:44:35 +0700

    Update

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-Facebook Open Graph Plugin for Mecha
-====================================
-
-Adding **Open Graph** meta tags to the `<head>` of your web page HTML will provide the crawler with structured info like a title, description and thumbnail image for the content.
+Facebook Open Graph Extension for Mecha
+=======================================
--- facebook-open-graph/index.php
+++ facebook-open-graph/index.php
@@ -1,4 +1,4 @@
-<?php namespace _;
+<?php namespace _\lot\x;
 
 function facebook_open_graph($content) {
     extract($GLOBALS, \EXTR_SKIP);
@@ -6,12 +6,12 @@ function facebook_open_graph($content) {
         $out  = '<!-- Begin Facebook Open Graph -->';
         $out .= '<meta property="og:title" content="' . \w($t->reverse) . '">';
         $out .= '<meta property="og:url" content="' . \r('&', '&amp;', $url->current) . '">';
-        if ($w = \w($page->description ?? $config->description ?? "")) {
+        if ($w = \w($page->description ?? $state->description ?? "")) {
             $out .= '<meta property="og:description" content="' . $w . '">';
         }
         $out .= '<meta property="og:image" content="' . ($page->image ?? $url . '/favicon.ico') . '">';
-        $out .= '<meta property="og:site_name" content="' . \w($config->title) . '">';
-        $out .= '<meta property="og:type" content="' . ($config->is('page') ? 'article' : 'website') . '">';
+        $out .= '<meta property="og:site_name" content="' . \w($state->title) . '">';
+        $out .= '<meta property="og:type" content="' . ($state->is('page') ? 'article' : 'website') . '">';
         $out .= '<!-- End Facebook Open Graph -->';
         return \str_replace('</head>', $out . '</head>', $content);
     }
