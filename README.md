f4331ea4c8932d512d7acd122e2fc30a2b7a6b40
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-09-22 00:08:05 +0700

    Update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Facebook Open Graph Extension for Mecha
 =======================================
+
+Release Notes
+-------------
+
+### 2.2.5
+
+ - Make title value clean.
--- facebook-open-graph/index.php
+++ facebook-open-graph/index.php
@@ -4,14 +4,14 @@ function facebook_open_graph($content) {
     extract($GLOBALS, \EXTR_SKIP);
     if (!empty($page)) {
         $out  = '<!-- Begin Facebook Open Graph -->';
-        $out .= '<meta property="og:title" content="' . \w($t->reverse) . '">';
+        $out .= '<meta property="og:title" content="' . \w($page->title ?? $t) . '">';
         $out .= '<meta property="og:url" content="' . \r('&', '&amp;', $url->current) . '">';
-        if ($w = \w($page->description ?? $state->description ?? "")) {
+        if ($w = \w($page->description ?? $site->description ?? "")) {
             $out .= '<meta property="og:description" content="' . $w . '">';
         }
         $out .= '<meta property="og:image" content="' . ($page->image ?? $url . '/favicon.ico') . '">';
-        $out .= '<meta property="og:site_name" content="' . \w($state->title) . '">';
-        $out .= '<meta property="og:type" content="' . ($state->is('page') ? 'article' : 'website') . '">';
+        $out .= '<meta property="og:site_name" content="' . \w($site->title) . '">';
+        $out .= '<meta property="og:type" content="' . ($site->is('page') ? 'article' : 'website') . '">';
         $out .= '<!-- End Facebook Open Graph -->';
         return \str_replace('</head>', $out . '</head>', $content);
     }
