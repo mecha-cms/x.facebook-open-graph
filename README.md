e92a65bfe98156c123ad8443b7bea02f9d9286dd
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-11 21:42:01 +0700

    Stable

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Facebook Open Graph Extension for [Mecha](https://github.com/mecha-cms/mecha)
 =============================================================================
 
 ![Code Size](https://img.shields.io/github/languages/code-size/mecha-cms/x.facebook-open-graph?color=%23444&style=for-the-badge)
+
+This extension adds Open Graph meta tags to the `<head>` section of your web page to provide the crawler with structured
+information such as the title, description, and image of the current page.
--- index.php
+++ index.php
@@ -1,23 +1,44 @@
 <?php namespace x\facebook_open_graph;
 
 function content($content) {
-    \extract($GLOBALS, \EXTR_SKIP);
-    if (!empty($page)) {
-        $out  = '<!-- Begin Facebook Open Graph -->';
-        if ($description = \w($page->description ?? $site->description ?? "")) {
-            $out .= '<meta content="' . \eat($description) . '" property="og:description">';
-        }
-        $out .= '<meta content="' . \eat($page->image ?? $url . '/favicon.ico') . '" property="og:image">';
-        $out .= '<meta content="' . \w($site->title) . '" property="og:site_name">';
-        if ($title = \w($page->title ?? $t ?? "")) {
-            $out .= '<meta content="' . $title . '" property="og:title">';
-        }
-        $out .= '<meta content="' . ($site->is('page') ? 'article' : 'website') . '" property="og:type">';
-        $out .= '<meta content="' . \eat($url->current) . '" property="og:url">';
-        $out .= '<!-- End Facebook Open Graph -->';
-        return \strtr($content, ['</head>' => $out . '</head>']);
+    if (!$content || false === \strpos($content, '</head>')) {
+        return $content;
     }
-    return $content;
+    \extract($GLOBALS, \EXTR_SKIP);
+    if (empty($page)) {
+        return $content;
+    }
+    $out = [false, [], []];
+    if ($description = \w($page->description ?? $site->description ?? "")) {
+        $out[1]['og:description'] = ['meta', false, [
+            'content' => $description,
+            'property' => 'og:description'
+        ]];
+    }
+    $out[1]['og:image'] = ['meta', false, [
+        'content' => $page->image ?? $url . '/favicon.ico',
+        'property' => 'og:image'
+    ]];
+    $out[1]['og:site_name'] = ['meta', false, [
+        'content' => \w($site->title),
+        'property' => 'og:site_name'
+    ]];
+    if ($title = \w($page->title ?? $t ?? "")) {
+        $out[1]['og:title'] = ['meta', false, [
+            'content' => $title,
+            'property' => 'og:title'
+        ]];
+    }
+    $out[1]['og:type'] = ['meta', false, [
+        'content' => $site->is('page') ? 'article' : 'website',
+        'property' => 'og:type'
+    ]];
+    $out[1]['og:url'] = ['meta', false, [
+        'content' => $url->current,
+        'property' => 'og:url'
+    ]];
+    $out = new \HTML(\Hook::fire('y.facebook-open-graph', [$out], $page), true);
+    return \strtr($content, ['</head>' => $out . '</head>']);
 }
 
 \Hook::set('content', __NAMESPACE__ . "\\content", 1.9);
