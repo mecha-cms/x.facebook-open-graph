71c09f8077889ae775ae9184cd33e5e94de0a2d6
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-03-19 08:23:17 +0700

    Use `lot()` Function

--- index.php
+++ index.php
@@ -4,41 +4,41 @@ function content($content) {
     if (!$content || false === \strpos($content, '</head>')) {
         return $content;
     }
-    \extract($GLOBALS, \EXTR_SKIP);
+    \extract(\lot(), \EXTR_SKIP);
     if (empty($page)) {
         return $content;
     }
-    $out = [false, [], []];
+    $y = [false, [], []];
     if ($description = \w($page->description ?? $site->description ?? "")) {
-        $out[1]['og:description'] = ['meta', false, [
+        $y[1]['og:description'] = ['meta', false, [
             'content' => $description,
             'property' => 'og:description'
         ]];
     }
-    $out[1]['og:image'] = ['meta', false, [
+    $y[1]['og:image'] = ['meta', false, [
         'content' => $page->image ?? $url . '/favicon.ico',
         'property' => 'og:image'
     ]];
-    $out[1]['og:site_name'] = ['meta', false, [
+    $y[1]['og:site_name'] = ['meta', false, [
         'content' => \w($site->title),
         'property' => 'og:site_name'
     ]];
     if ($title = \w($page->title ?? $t ?? "")) {
-        $out[1]['og:title'] = ['meta', false, [
+        $y[1]['og:title'] = ['meta', false, [
             'content' => $title,
             'property' => 'og:title'
         ]];
     }
-    $out[1]['og:type'] = ['meta', false, [
+    $y[1]['og:type'] = ['meta', false, [
         'content' => $site->is('page') ? 'article' : 'website',
         'property' => 'og:type'
     ]];
-    $out[1]['og:url'] = ['meta', false, [
+    $y[1]['og:url'] = ['meta', false, [
         'content' => $url->current,
         'property' => 'og:url'
     ]];
-    $out = new \HTML(\Hook::fire('y.facebook-open-graph', [$out], $page), true);
-    return \strtr($content, ['</head>' => $out . '</head>']);
+    $v = new \HTML(\Hook::fire('y.facebook-open-graph', [$y], $page), true);
+    return \strtr($content, ['</head>' => $v . '</head>']);
 }
 
 \Hook::set('content', __NAMESPACE__ . "\\content", 1.9);
